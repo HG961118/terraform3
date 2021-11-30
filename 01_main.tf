033324b480f9de9546b7a08e67369d035a1dc20f
unknown <nikitakim21@gmail.com>
2021-11-30 12:06:55 +0900

    'step-1-all'

--- 01_main.tf
+++ 01_main.tf
@@ -1,12 +1,55 @@
 provider "aws" {
-    region = "ap-northeast-2"
+    region = var.region
 }
 
+resource "aws_key_pair" "hgkim_key" {
+    key_name = var.key
+    public_key = file("../../.ssh/hgkim_key.pub")
+   
+}
+
+
 resource "aws_vpc" "hgkim_vpc" {
-    cidr_block          = "10.0.0.0/16"
-    enable_dns_hostname = true
+    cidr_block          = var.cidr_main
+    enable_dns_hostnames = true
     enable_dns_support  = true
     tags = {
-        "Name" = "hgkim-vpc"
+        "Name" = "${var.name}-vpc"
     }
 }
+
+
+#가용 영역 a의 public subnet
+resource "aws_subnet" "hgkim_pub" {
+  count = length(var.pub_sub)      #2
+  vpc_id = aws_vpc.hgkim_vpc.id
+  cidr_block = var.pub_sub[count.index]  #index 0 -> 10.0.0.0/24
+  availability_zone = "${var.region}${var.ava_zone[count.index]}"
+  tags = {
+    "Name" = "${var.name}--pub${var.ava_zone[count.index]}"
+  }
+}
+
+
+resource "aws_subnet" "hgkim_pri" {
+  count = 2
+  vpc_id = aws_vpc.hgkim_vpc.id
+  cidr_block = var.pri_sub[count.index] 
+  availability_zone = "${var.region}${var.ava_zone[count.index]}"
+  tags = {
+    "Name" = "${var.name}-pri{var.ava_zone[count.index]}"
+  }
+}
+
+
+resource "aws_subnet" "hgkim_pridb" {
+    count = length(var.pri_subdb) #2
+  vpc_id = aws_vpc.hgkim_vpc.id
+  cidr_block = var.pri_subdb[count.index]
+  availability_zone = "${var.region}${var.ava_zone[count.index]}"
+  tags = {
+    "Name" = "${var.name}-pridb{var.ava_zone[count.index]}"
+  }
+}
+
+
